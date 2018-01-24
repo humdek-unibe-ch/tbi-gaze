a72998366708a0d063616251b0b54098892c6b1e
Simon Maurer <simon.maurer@humdek.unibe.ch>
2018-01-24 11:31:56 +0100

    add comments and close process

--- gaze2mouse/Program.cs
+++ gaze2mouse/Program.cs
@@ -1,15 +1,18 @@
 ﻿/**
+ * Converts the stream of gaze points, captured by a Tobii eyetracker, to mouse cursor location
  * 
+ * @file    gaze2mouse.cs
+ * @author  Simon Maurer, simon.maurer@humdek.unibe.ch
+ * @date    January 2018
  */
 
 using System;
 using System.IO;
-using System.Threading;
 using System.Windows.Forms;
 using Tobii.Interaction;
 using Tobii.Interaction.Framework;
 
-namespace gazeDataStream
+namespace gaze2mouse
 {
     class Program
     {
@@ -20,8 +23,21 @@ namespace gazeDataStream
         private static bool hasRun = false;
         private static Host host;
         
+        /**
+         * @brief Helper class to be added to the application's message pump to filter out a message
+         */
         private class TestMessageFilter : IMessageFilter
         {
+            /**
+             * @brief filters out a message before it is dispatched
+             * 
+             * The current implementation only cares about the signal that is sent to the application by taskkill (WM_CLOSE).
+             * Once WM_CLOSE is received, the event "ApplicationExit" is invoked.
+             * 
+             * @param m the pre-filtered message
+             * @return  true if the received message corresponds to WM_CLOSE
+             *          false if any other message is pre-filtered
+             */
             public bool PreFilterMessage(ref Message m)
             {
                 if (m.Msg == /*WM_CLOSE*/ 0x10)
@@ -33,6 +49,9 @@ namespace gazeDataStream
             }
         }
         
+        /**
+         * @brief The main programm entry
+         */
         static void Main(string[] args)
         {
             // open files
@@ -45,28 +64,47 @@ namespace gazeDataStream
 
             // initialize host. Make sure that the Tobii service is running
             host = new Host();
+            Cursor.Hide();
 
             // create stream
             var gazePointDataStream = host.Streams.CreateGazePointDataStream(GazePointDataMode.Unfiltered);
+            // whenever a new gaze point is available, run gaze2mouse
             gazePointDataStream.GazePoint((x, y, ts) => gaze2mouse( x, y, ts ));
-
+            
+            // add message filter to the application's message pump
             Application.AddMessageFilter(new TestMessageFilter());
             Application.Run();
         }
 
+        /**
+         * @brief set the mouse pointer to the location of the gaze point and log the coordiantes
+         * 
+         * @param x     the x-coordinate of the gaze point
+         * @param y     the y-coordinate of the gaye point
+         * @param ts    the timestamp of the the capture instant of the gaye point
+         *              Note that the timestamp reference represents an arbitrary point in time
+         */
         static void gaze2mouse(double x, double y, double ts)
         {
+            // create a time reference that corresponds to the local machine
             TimeSpan ts_rec = TimeSpan.FromMilliseconds(ts);
             if (!hasRun) ts_delta = ts_rec - ts_start;
             ts_rec -= ts_delta;
+
+            // set the cursor position to the gaze position
             Cursor.Position = new System.Drawing.Point( Convert.ToInt32(x), Convert.ToInt32(y) );
+
+            // write the coordinates to the log file
             sw.WriteLine("{0}\t{1:0.0}\t{2:0.0}", ts_rec, x, y);
             hasRun = true;
         }
 
+        /**
+         * @brief cleanup. To be run on application exit. 
+         */
         static void OnApplicationExit(object sender, EventArgs e)
         {
-            sw.WriteLine("Programm was terminated");
+            sw.WriteLine("done");
             sw.Close();
             fs.Close();
             sw.Dispose();
---
+++ gaze2mouse_close/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace gaze2mouse_close
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Cursor.Show();
+            Process.Start("taskkill", "/IM gaze2mouse.exe");
+        }
+    }
+}
