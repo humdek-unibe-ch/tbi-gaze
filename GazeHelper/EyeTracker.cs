a3c31bc1e95773292ce275bc66d51ed5db651559
Simon Maurer <simon.maurer@humdek.unibe.ch>
2018-02-16 17:32:33 +0100

    working with tobii api to check tracker state before accessing it

---
+++ GazeHelper/EyeTracker.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using System.Threading;
+using Tobii.Interaction;
+using Tobii.Interaction.Framework;
+using GazeHelper;
+
+namespace GazeHelper
+{
+    public class EyeTracker
+    {
+        private const int MAX_WAIT_TIME = 10000;    // 10 seconds
+        private const int PERIOD_WAIT_TIME = 500;   // 0.5 seconds
+        private EyeTrackingDeviceStatus state;
+        private Logger logger;
+
+        public EyeTracker(Logger logger)
+        {
+            this.logger = logger;
+            Host host = new Host();
+            var deviceStateObserver = host.States.CreateEyeTrackingDeviceStatusObserver();
+            deviceStateObserver.WhenChanged(deviceState => UpdateDeviceStatus(deviceState));
+        }
+
+        public delegate void IsReady();
+        public delegate void IsNotReady(string state);
+
+        public void WhenReady( IsReady sucess, IsNotReady fail)
+        {
+            bool ready = false;
+            int counter = 0;
+            while (counter < MAX_WAIT_TIME)
+            {
+                counter += PERIOD_WAIT_TIME;
+                logger.Debug($"Checking state: {state}");
+                if (state == EyeTrackingDeviceStatus.Tracking)
+                {
+                    ready = true;
+                    break;
+                }
+                Thread.Sleep(PERIOD_WAIT_TIME);
+            }
+            if (ready) sucess();
+            else fail(state.ToString());
+        }
+
+        private void UpdateDeviceStatus( EngineStateValue<EyeTrackingDeviceStatus> deviceState )
+        {
+            state = deviceState.Value;
+            logger.Debug($"Eye Tracker changed state: {state}");
+        }
+    }
+}
--- TobiiGuestCalibrate/TobiiGuestCalibrate.cs
+++ TobiiGuestCalibrate/TobiiGuestCalibrate.cs
@@ -19,10 +19,15 @@ namespace TobiiGuestCalibrate
         static void Main()
         {
             Logger logger = new Logger();
-            JsonConfigParser parser = new JsonConfigParser(logger);
-            JsonConfigParser.ConfigItem item = parser.ParseJsonConfig();
-            logger.Info($"Starting Tobii guest calibration \"{item.TobiiPath}\\{item.TobiiGuestCalibrate} {item.TobiiGuestCalibrateArguments}\"");
-            Process.Start($"{item.TobiiPath}\\{item.TobiiGuestCalibrate}", item.TobiiGuestCalibrateArguments);
+            EyeTracker tracker = new EyeTracker(logger);
+            tracker.WhenReady(() => {
+                JsonConfigParser parser = new JsonConfigParser(logger);
+                JsonConfigParser.ConfigItem item = parser.ParseJsonConfig();
+                logger.Info($"Starting Tobii guest calibration \"{item.TobiiPath}\\{item.TobiiGuestCalibrate} {item.TobiiGuestCalibrateArguments}\"");
+                Process.Start($"{item.TobiiPath}\\{item.TobiiGuestCalibrate}", item.TobiiGuestCalibrateArguments);
+            }, state => {
+                logger.Error($"The eye tracker device is not ready. Its state is \"{state}\"");
+            });
         }
     }
 }
