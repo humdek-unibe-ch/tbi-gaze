097feaf15e3ce84fb5cdef9136dec0ed734a4c26
Simon Maurer <simon.maurer@unibe.ch>
2023-08-17 17:44:03 +0200

    change create shorcut helper script to generate batch scripts instead of links

--- scripts/CreateShortcuts.bat
+++ scripts/CreateShortcuts.bat
@@ -3,19 +3,24 @@ SETLOCAL
 
 SET source=%CD%\GazeControl.exe
 
-CALL :CreateShortcut CUSTOM_CALIBRATE , CustomCalibrate
-CALL :CreateShortcut DRIFT_COMPENSATION , DriftCompensation
-CALL :CreateShortcut TERMINATE , GazeClose
-CALL :CreateShortcut GAZE_RECORDING_DISABLE , GazeRecordingDisable
-CALL :CreateShortcut GAZE_RECORDING_ENABLE , GazeRecordingEnable
-CALL :CreateShortcut MOUSE_TRACKING_DISABLE , MouseTrackingDisable
-CALL :CreateShortcut MOUSE_TRACKING_ENABLE , MouseTrackingEnable
-CALL :CreateShortcut RESET_DRIFT_COMPENSATION , ResetDriftCompensation
-CALL :CreateShortcut RESET_START_TIME , ResetStartTime
+CALL :CreateCmd CUSTOM_CALIBRATE , CustomCalibrate
+CALL :CreateCmd DRIFT_COMPENSATION , DriftCompensation
+CALL :CreateCmd TERMINATE , GazeClose
+CALL :CreateCmd GAZE_RECORDING_DISABLE , GazeRecordingDisable
+CALL :CreateCmd GAZE_RECORDING_ENABLE , GazeRecordingEnable
+CALL :CreateCmd MOUSE_TRACKING_DISABLE , MouseTrackingDisable
+CALL :CreateCmd MOUSE_TRACKING_ENABLE , MouseTrackingEnable
+CALL :CreateCmd RESET_DRIFT_COMPENSATION , ResetDriftCompensation
+CALL :CreateCmd RESET_START_TIME , ResetStartTime
 
 EXIT /B %ERRORLEVEL%
 
 :CreateShortcut
 DEL ".\%~2.lnk"
 POWERSHELL -ExecutionPolicy unrestricted -File .\CreateShortcut.ps1 "%source%" "%CD%" "/command %~1" ".\%~2.lnk"
+EXIT /B 0
+
+:CreateCmd
+DEL ".\%~2.cmd"
+ECHO START %source% /command %~1 > "%~2.cmd"
 EXIT /B 0
