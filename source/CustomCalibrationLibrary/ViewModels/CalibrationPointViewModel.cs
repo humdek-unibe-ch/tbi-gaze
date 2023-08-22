humdek-unibe-ch/tbi-gaze

commit dae68e72d696219f440cca8c8a1a4252d3585f33

document code

source/CustomCalibrationLibrary/ViewModels/CalibrationPointViewModel.cs
@@ -8,11 +8,11 @@ namespace CustomCalibrationLibrary.ViewModels
     /// </summary>
     class CalibrationPointViewModel : CalibrationPoint
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CalibrationPointViewModel"/> class.
+        /// </summary>
+        /// <param name="position">The position of the calibration point.</param>
+        /// <param name="index">The index of the calibration point.</param>
         public CalibrationPointViewModel(Point point, int index) : base(point, index) { }
-        public CalibrationPointViewModel(CalibrationPoint model) : base(model.Position, model.Index) {
-            GazePositionAverage = model.GazePositionAverage;
-            GazePositionLeft = model.GazePositionLeft;
-            GazePositionRight = model.GazePositionRight;
-        }
     }
 }

source/CustomCalibrationLibrary/ViewModels/CalibrationViewModel.cs
@@ -33,7 +33,7 @@ namespace CustomCalibrationLibrary.ViewModels
             _model = model;
             foreach (CalibrationPoint item in _model.CalibrationPoints)
             {
-                _calibrationPoints.Add(new CalibrationPointViewModel(item));
+                _calibrationPoints.Add(new CalibrationPointViewModel(item.Position, item.Index));
                 item.PropertyChanged += OnCollectionItemChanged;
             }
             _model.CalibrationPoints.CollectionChanged += OnCollectionChanged;

source/CustomCalibrationLibrary/Commands/CalibrationCommand.cs
@@ -11,23 +11,40 @@ namespace CustomCalibrationLibrary.Commands
     {
         private CalibrationEventType _eventType;
         private CalibrationModel _model;
+        /// <summary>
+        /// Event handler on can executed flag change.
+        /// </summary>
         public event EventHandler? CanExecuteChanged
         {
             add { }
             remove { }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CalibrationCommand"/> class.
+        /// </summary>
+        /// <param name="model">The calibration model</param>
+        /// <param name="eventType">The type of the calibration event.</param>
         public CalibrationCommand(CalibrationModel model, CalibrationEventType eventType)
         {
             _model = model;
             _eventType = eventType;
         }
 
+        /// <summary>
+        /// Returns whether command can be executed or not.
+        /// </summary>
+        /// <param name="parameter">The command parameter</param>
+        /// <returns>True</returns>
         public bool CanExecute(object? parameter)
         {
             return true;
         }
 
+        /// <summary>
+        /// Send calibration event.
+        /// </summary>
+        /// <param name="parameter">The command parameter</param>
         public void Execute(object? parameter)
         {
             _model.OnCalibrationEvent(_eventType);

source/CustomCalibrationLibrary/Commands/GazeVisibilityCommand.cs
@@ -10,22 +10,38 @@ namespace CustomCalibrationLibrary.Commands
     internal class GazeVisibilityCommand : ICommand
     {
         private CalibrationResultViewModel _model;
+        /// <summary>
+        /// Event handler on can executed flag change.
+        /// </summary>
         public event EventHandler? CanExecuteChanged
         {
             add { }
             remove { }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CalibrationCommand"/> class.
+        /// </summary>
+        /// <param name="model">The calibration result view model</param>
         public GazeVisibilityCommand(CalibrationResultViewModel model)
         {
             _model = model;
         }
 
+        /// <summary>
+        /// Returns whether command can be executed or not.
+        /// </summary>
+        /// <param name="parameter">The command parameter</param>
+        /// <returns>True</returns>
         public bool CanExecute(object? parameter)
         {
             return true;
         }
 
+        /// <summary>
+        /// Toggle the gaze visibility.
+        /// </summary>
+        /// <param name="parameter">The command parameter</param>
         public void Execute(object? parameter)
         {
             _model.OnGazeToggle();

source/CustomCalibrationLibrary/Converters/HasDataToVisibilityConverter.cs
@@ -13,6 +13,15 @@ namespace CustomCalibrationLibrary.Converters
     /// </summary>
     public class HasDataToVisibilityConverter: IValueConverter
     {
+        /// <summary>
+        /// Value converter.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="targetType">The type of the target value.</param>
+        /// <param name="parameter">The conversion parameter.</param>
+        /// <param name="culture">The language localisation.</param>
+        /// <returns>The converted value object</returns>
+        /// <exception cref="InvalidOperationException"></exception>
         public object Convert(object value, Type targetType, object parameter,
             System.Globalization.CultureInfo culture)
         {
@@ -22,6 +31,15 @@ namespace CustomCalibrationLibrary.Converters
             return (bool)value ? Visibility.Hidden : Visibility.Visible;
         }
 
+        /// <summary>
+        /// Reverted value converter.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="targetType">The type of the target value.</param>
+        /// <param name="parameter">The conversion parameter.</param>
+        /// <param name="culture">The language localisation.</param>
+        /// <returns>The converted value object</returns>
+        /// <exception cref="NotSupportedException"></exception>
         public object ConvertBack(object value, Type targetType, object parameter,
             System.Globalization.CultureInfo culture)
         {

source/CustomCalibrationLibrary/Converters/PositionConverter.cs
@@ -10,15 +10,29 @@ namespace CustomCalibrationLibrary.Converters
     /// </summary>
     public class PositionConverter: DependencyObject, IValueConverter
     {
+        /// <summary>
+        /// The position offset.
+        /// </summary>
         public string Offset
         {
             get => (string)GetValue(OffsetProperty) ?? "0";
             set => SetValue(OffsetProperty, value);
         }
 
+        /// <summary>
+        /// The custom offset property of the value converter.
+        /// </summary>
         public static readonly DependencyProperty OffsetProperty =
         DependencyProperty.Register("Offset", typeof(string), typeof(PositionConverter), new PropertyMetadata(null));
 
+        /// <summary>
+        /// Value converter.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="targetType">The type of the target value.</param>
+        /// <param name="parameter">The conversion parameter.</param>
+        /// <param name="culture">The language localisation.</param>
+        /// <returns>The converted value object</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value == null)
@@ -46,6 +60,15 @@ namespace CustomCalibrationLibrary.Converters
             return 0;
         }
 
+        /// <summary>
+        /// Reverted value converter.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="targetType">The type of the target value.</param>
+        /// <param name="parameter">The conversion parameter.</param>
+        /// <param name="culture">The language localisation.</param>
+        /// <returns>The converted value object</returns>
+        /// <exception cref="NotSupportedException"></exception>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();

source/CustomCalibrationLibrary/Converters/ProximityColorConverter.cs
@@ -11,7 +11,14 @@ namespace CustomCalibrationLibrary.Converters
     /// </summary>
     public class ProximityColorConverter : IValueConverter
     {
-
+        /// <summary>
+        /// Value converter.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="targetType">The type of the target value.</param>
+        /// <param name="parameter">The conversion parameter.</param>
+        /// <param name="culture">The language localisation.</param>
+        /// <returns>The converted value object</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value == null)
@@ -40,9 +47,18 @@ namespace CustomCalibrationLibrary.Converters
             return new SolidColorBrush(Colors.White);
         }
 
+        /// <summary>
+        /// Reverted value converter.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="targetType">The type of the target value.</param>
+        /// <param name="parameter">The conversion parameter.</param>
+        /// <param name="culture">The language localisation.</param>
+        /// <returns>The converted value object</returns>
+        /// <exception cref="NotSupportedException"></exception>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return 0;
+            throw new NotImplementedException();
         }
     }
 }

source/CustomCalibrationLibrary/Models/CalibrationModel.cs
@@ -48,17 +48,26 @@ namespace CustomCalibrationLibrary.Models
         public string Error { get { return _error; } set { _error = value; OnPropertyChanged(); } }
 
         /// <summary>
-        /// Events to trigger changes in the calibration process.
+        /// Event to trigger changes in the calibration process.
         /// </summary>
         public event EventHandler<CalibrationEventType>? CalibrationEvent;
+        /// <summary>
+        /// The calibraion event change handler.
+        /// </summary>
+        /// <param name="type"></param>
         public void OnCalibrationEvent(CalibrationEventType type)
         {
             CalibrationEvent?.Invoke(this, type);
         }
-
+        /// <summary>
+        /// Event to trigger property changes in this class.
+        /// </summary>
         public event PropertyChangedEventHandler? PropertyChanged;
 
         private GazeValidationData _validationData;
+        /// <summary>
+        /// The data returned by a successful validation process.
+        /// </summary>
         public GazeValidationData ValidationData
         {
             get { return _validationData; }
@@ -122,6 +131,9 @@ namespace CustomCalibrationLibrary.Models
             GazePointChanged?.Invoke(this, _gazePoint);
         }
 
+        /// <summary>
+        /// Event to trigger user position guide changes.
+        /// </summary>
         public event EventHandler<UserPositionData>? UserPositionGuideChanged;
         private UserPositionData _userPositionGuide;
         /// <summary>
@@ -151,7 +163,11 @@ namespace CustomCalibrationLibrary.Models
 
         private TrackerLogger _logger;
 
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CalibrationModel"/> class.
+        /// </summary>
+        /// <param name="logger">The log handler.</param>
+        /// <param name="points">Calibration points.</param>
         public CalibrationModel(TrackerLogger logger, double[][] points)
         {
             _logger = logger;

commit 683b5a10b8729944a5a2582834d74be949998cda

fix bug introduced with commit dae68e72

source/CustomCalibrationLibrary/ViewModels/CalibrationPointViewModel.cs
@@ -14,5 +14,17 @@ namespace CustomCalibrationLibrary.ViewModels
         /// <param name="point">The position of the calibration point.</param>
         /// <param name="index">The index of the calibration point.</param>
         public CalibrationPointViewModel(Point point, int index) : base(point, index) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CalibrationPointViewModel"/> class.
+        /// </summary>
+        /// <param name="point">The calibration point object.</param>
+        public CalibrationPointViewModel(CalibrationPoint point) : base(point.Position, point.Index)
+        {
+            GazePositionAverage = point.GazePositionAverage;
+            GazePositionLeft = point.GazePositionLeft;
+            GazePositionRight = point.GazePositionRight;
+        }
+
     }
 }

source/CustomCalibrationLibrary/ViewModels/CalibrationViewModel.cs
@@ -36,7 +36,7 @@ namespace CustomCalibrationLibrary.ViewModels
             _model = model;
             foreach (CalibrationPoint item in _model.CalibrationPoints)
             {
-                _calibrationPoints.Add(new CalibrationPointViewModel(item.Position, item.Index));
+                _calibrationPoints.Add(new CalibrationPointViewModel(item));
                 item.PropertyChanged += OnCollectionItemChanged;
             }
             _model.CalibrationPoints.CollectionChanged += OnCollectionChanged;