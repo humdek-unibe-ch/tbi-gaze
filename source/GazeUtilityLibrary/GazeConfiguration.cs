6ac1c5edfed2beb1116d393c4e43b5b273c37c8c
Simon Maurer <simon.maurer@unibe.ch>
2023-11-21 11:36:17 +0100

    restore license header

--- source/GazeUtilityLibrary/GazeConfiguration.cs
+++ source/GazeUtilityLibrary/GazeConfiguration.cs
@@ -1,4 +1,9 @@
-﻿using GazeUtilityLibrary.DataStructs;
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+using GazeUtilityLibrary.DataStructs;
 using Newtonsoft.Json;
 using System;
 using System.IO;
