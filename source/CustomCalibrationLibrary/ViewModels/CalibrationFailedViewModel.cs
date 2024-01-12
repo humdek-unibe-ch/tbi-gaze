ed9f66393180a7019673fb8237a69f0a52324eec
Simon Maurer <simon.maurer@unibe.ch>
2024-01-12 12:27:10 +0100

    fix background color on calibration fail

---
+++ source/CustomCalibrationLibrary/ViewModels/CalibrationFailedViewModel.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+using CustomCalibrationLibrary.Commands;
+using CustomCalibrationLibrary.Models;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CustomCalibrationLibrary.ViewModels
+{
+    public class CalibrationFailedViewModel : ColoredViewModel
+    {
+        private CalibrationModel _model;
+
+        private ICommand _calibrationRestartCommand;
+        /// <summary>
+        /// Command to restart the calibration
+        /// </summary>
+        public ICommand CalibrationRestartCommand { get { return _calibrationRestartCommand; } }
+
+        private ICommand _calibrationAbortCommand;
+        /// <summary>
+        /// Command to abort the calibration
+        /// </summary>
+        public ICommand CalibrationAbortCommand { get { return _calibrationAbortCommand; } }
+
+        /// <summary>
+        /// The property change event to update the view.
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string _error;
+        /// <summary>
+        /// The error message to be updated on the view.
+        /// </summary>
+        public string Error { get { return _error; } set { _error = value; OnPropertyChanged(); } }
+
+        /// <summary>
+        /// The property change handler to update the view.
+        /// </summary>
+        /// <param name="property_name">The name of the property to update</param>
+        private void OnPropertyChanged([CallerMemberName] string? property_name = null)
+        {
+            Application.Current.Dispatcher.Invoke(() => {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property_name));
+            });
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="model">The calibration model</param>
+        public CalibrationFailedViewModel(CalibrationModel model) : base(model.BackgroundColor, model.FrameColor)
+        {
+            _error = model.Error;
+            _model = model;
+            _calibrationRestartCommand = new CalibrationCommand(model, CalibrationEventType.Restart);
+            _calibrationAbortCommand = new CalibrationCommand(model, CalibrationEventType.Abort);
+        }
+
+        private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (sender == null || e.PropertyName != "Error")
+            {
+                return;
+            }
+            Error = ((CalibrationModel)sender).Error;
+        }
+    }
+}
--- source/CustomCalibrationLibrary/Views/CalibrationFailed.xaml.cs
+++ source/CustomCalibrationLibrary/Views/CalibrationFailed.xaml.cs
@@ -5,6 +5,7 @@
  */
 ﻿using CustomCalibrationLibrary.Commands;
 using CustomCalibrationLibrary.Models;
+using CustomCalibrationLibrary.ViewModels;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
@@ -16,43 +17,8 @@ namespace CustomCalibrationLibrary.Views
     /// <summary>
     /// Interaction logic for CalibrationFailed.xaml
     /// </summary>
-    public partial class CalibrationFailed : UserControl, INotifyPropertyChanged
+    public partial class CalibrationFailed : UserControl
     {
-        private CalibrationModel _model;
-
-        private ICommand _calibrationRestartCommand;
-        /// <summary>
-        /// Command to restart the calibration
-        /// </summary>
-        public ICommand CalibrationRestartCommand { get { return _calibrationRestartCommand; } }
-
-        private ICommand _calibrationAbortCommand;
-        /// <summary>
-        /// Command to abort the calibration
-        /// </summary>
-        public ICommand CalibrationAbortCommand { get { return _calibrationAbortCommand; } }
-
-        /// <summary>
-        /// The property change event to update the view.
-        /// </summary>
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        private string _error;
-        /// <summary>
-        /// The error message to be updated on the view.
-        /// </summary>
-        public string Error { get { return _error; } set { _error = value; OnPropertyChanged(); } }
-
-        /// <summary>
-        /// The property change handler to update the view.
-        /// </summary>
-        /// <param name="property_name">The name of the property to update</param>
-        private void OnPropertyChanged([CallerMemberName] string? property_name = null)
-        {
-            Application.Current.Dispatcher.Invoke(() => {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property_name));
-            });
-        }
 
         /// <summary>
         /// Constructor
@@ -60,22 +26,8 @@ namespace CustomCalibrationLibrary.Views
         /// <param name="model">The claibration model</param>
         public CalibrationFailed(CalibrationModel model)
         {
-            _error = model.Error;
-            _model = model;
-            _model.PropertyChanged += OnPropertyChanged;
             InitializeComponent();
-            DataContext = this;
-            _calibrationRestartCommand = new CalibrationCommand(model, CalibrationEventType.Restart);
-            _calibrationAbortCommand = new CalibrationCommand(model, CalibrationEventType.Abort);
-        }
-
-        private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
-        {
-            if (sender == null || e.PropertyName != "Error")
-            {
-                return;
-            }
-            Error = ((CalibrationModel)sender).Error;
+            DataContext = new CalibrationFailedViewModel(model);
         }
 
         private void OnLoaded(object sender, System.Windows.RoutedEventArgs e)
