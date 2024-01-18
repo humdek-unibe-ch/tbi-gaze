7a68845820ca77edd832394295ab980f7dedffc6
Simon Maurer <simon.maurer@unibe.ch>
2024-01-18 21:00:37 +0100

    first success with systray icon

---
+++ source/GazeToMouse/Commands/StartDriftCompensationCommand.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+using System;
+using System.Windows.Input;
+
+namespace GazeToMouse.Commands
+{
+    /// <summary>
+    /// Comand class to trigger calibration events.
+    /// </summary>
+    public class StartDriftCompensationCommand : ICommand
+    {
+        private App _app;
+        /// <summary>
+        /// Event handler on can executed flag change.
+        /// </summary>
+        public event EventHandler? CanExecuteChanged
+        {
+            add { }
+            remove { }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExitApplicationCommand"/> class.
+        /// </summary>
+        /// <param name="app">The main application</param>
+        public StartDriftCompensationCommand(App app)
+        {
+            _app = app;
+        }
+
+        /// <summary>
+        /// Returns whether command can be executed or not.
+        /// </summary>
+        /// <param name="parameter">The command parameter</param>
+        /// <returns>True</returns>
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Send calibration event.
+        /// </summary>
+        /// <param name="parameter">The command parameter</param>
+        public void Execute(object? parameter)
+        {
+            _app.CustomDispatcher.Invoke(() => _app.CompensateDrift());
+        }
+    }
+}
---
+++ source/GazeToMouse/NotifyIconViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Input;
+using GazeToMouse.Commands;
+
+namespace GazeToMouse;
+
+/// <summary>
+/// Provides bindable properties and commands for the NotifyIcon. In this sample, the
+/// view model is assigned to the NotifyIcon in XAML. Alternatively, the startup routing
+/// in App.xaml.cs could have created this view model, and assigned it to the NotifyIcon.
+/// </summary>
+public partial class NotifyIconViewModel
+{
+    private ICommand _exitApplicationCommand;
+    /// <summary>
+    /// Command to exit the application
+    /// </summary>
+    public ICommand ExitApplicationCommand { get { return _exitApplicationCommand; } }
+
+    private ICommand _startCalibrationCommand;
+    /// <summary>
+    /// Command to start the calibration
+    /// </summary>
+    public ICommand StartCalibrationCommand { get { return _startCalibrationCommand; } }
+
+    private ICommand _startValidationCommand;
+    /// <summary>
+    /// Command to start the validation
+    /// </summary>
+    public ICommand StartValidationCommand { get { return _startValidationCommand; } }
+
+    private ICommand _startDriftCompensationCommand;
+    /// <summary>
+    /// Command to start the validation
+    /// </summary>
+    public ICommand StartDriftCompensationCommand { get { return _startDriftCompensationCommand; } }
+
+    public NotifyIconViewModel(App app)
+    {
+        _exitApplicationCommand = new ExitApplicationCommand(app);
+        _startCalibrationCommand = new StartCalibrationCommand(app);
+        _startValidationCommand = new StartValidationCommand(app);
+        _startDriftCompensationCommand = new StartDriftCompensationCommand(app);
+    }
+}
